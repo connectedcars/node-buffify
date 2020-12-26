f41579c686342d14c08c7f4c83df292c1daf1732
Troels Liebe Bentsen <troels@connectedcars.io>
2020-12-26 22:47:32 +0100

    Add support for undefined, replacer and indentation

--- src/buffify.ts
+++ src/buffify.ts
@@ -1,15 +1,28 @@
 import crypto from 'crypto'
 
 // https://github.com/microsoft/TypeScript/issues/1897
-export type Json = null | boolean | number | string | Json[] | { [prop: string]: Json }
+export type Json = null | boolean | number | string | Json[] | { [prop: string]: Json | undefined }
+export type JsonReplacer = (
+  this: { [prop: string]: Json | undefined },
+  key: string,
+  value: Json,
+  level: number,
+  keyPath: string
+) => Json | undefined
 
-const arrayOpen = Buffer.from('[', 'utf8')
-const arrayClose = Buffer.from(']', 'utf8')
-const objectOpen = Buffer.from('{', 'utf8')
-const objectClose = Buffer.from('}', 'utf8')
-const comma = Buffer.from(',', 'utf8')
+export function jsonBuffify(json: Json, replace?: JsonReplacer | null, space: number | string = ''): Buffer {
+  return _jsonBuffify(
+    json,
+    replace ? replace : (_, value) => value,
+    typeof space === 'number' ? ' '.repeat(space) : space
+  )
+}
 
-export function jsonBuffify(json: Json): Buffer {
+function _jsonBuffify(json: Json, replace: JsonReplacer, space: string, level = 1, keyPath = ''): Buffer {
+  const propIndentation = space.length > 0 ? space.repeat(level) : ''
+  const endIndentation = space.length > 0 && level > 1 ? space.repeat(level - 1) : ''
+  const keyValueSpace = space.length > 0 ? ' ' : ''
+  const lineBreak = space.length > 0 ? '\n' : ''
   if (json === null) {
     return Buffer.from('null', 'utf8')
   } else {
@@ -23,26 +36,38 @@ export function jsonBuffify(json: Json): Buffer {
     } else if (type === 'object') {
       const values: Buffer[] = []
       if (Array.isArray(json)) {
-        values.push(arrayOpen)
-        for (const [i, value] of json.entries()) {
-          values.push(jsonBuffify(value))
-          if (i !== json.length - 1) {
-            values.push(comma)
+        if (json.length > 0) {
+          values.push(Buffer.from('[' + lineBreak, 'utf8'))
+          for (const [i, value] of json.entries()) {
+            if (values.length > 1) {
+              values.push(Buffer.from(',' + lineBreak, 'utf8'))
+            }
+            values.push(Buffer.from(propIndentation))
+            values.push(_jsonBuffify(value, replace, space, level + 1, `${keyPath}[${i}]`))
           }
+          values.push(Buffer.from(lineBreak + endIndentation + ']', 'utf8'))
+        } else {
+          values.push(Buffer.from('[]', 'utf8'))
         }
-        values.push(arrayClose)
       } else {
-        values.push(objectOpen)
         const obj = json as { [key: string]: Json }
         const keys = Object.keys(json).sort()
-        for (const [i, key] of keys.entries()) {
-          values.push(Buffer.from(JSON.stringify(key) + ':', 'utf8'))
-          values.push(jsonBuffify(obj[key]))
-          if (i !== keys.length - 1) {
-            values.push(comma)
+        if (keys.length > 0) {
+          values.push(Buffer.from('{' + lineBreak, 'utf8'))
+          for (const key of keys) {
+            const value = replace.bind(obj)(key, obj[key], level, `${keyPath}.${key}`)
+            if (typeof value !== 'undefined') {
+              if (values.length > 1) {
+                values.push(Buffer.from(',' + lineBreak, 'utf8'))
+              }
+              values.push(Buffer.from(propIndentation + JSON.stringify(key) + ':' + keyValueSpace, 'utf8'))
+              values.push(_jsonBuffify(value, replace, space, level + 1, `${keyPath}.${key}`))
+            }
           }
+          values.push(Buffer.from(lineBreak + endIndentation + '}', 'utf8'))
+        } else {
+          values.push(Buffer.from('{}', 'utf8'))
         }
-        values.push(objectClose)
       }
       return Buffer.concat(values)
     } else {
@@ -51,8 +76,13 @@ export function jsonBuffify(json: Json): Buffer {
   }
 }
 
-export function jsonDigest(json: Json, algorithm = 'sha256'): Buffer {
+export function jsonDigest(
+  json: Json,
+  algorithm = 'sha256',
+  replace?: JsonReplacer | null,
+  space: number | string = ''
+): Buffer {
   const hash = crypto.createHash(algorithm)
-  hash.update(jsonBuffify(json))
+  hash.update(jsonBuffify(json, replace, space))
   return hash.digest()
 }
